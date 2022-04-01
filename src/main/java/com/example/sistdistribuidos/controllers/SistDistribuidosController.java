6602dc7da271614aa487e165a4748a28f8064ed0
fveloci <fveloci@hotmail.com>
2022-04-01 16:44:16 -0300

    Add sumar

--- src/main/java/com/example/sistdistribuidos/controllers/SistDistribuidosController.java
+++ src/main/java/com/example/sistdistribuidos/controllers/SistDistribuidosController.java
@@ -1,6 +1,7 @@
 package com.example.sistdistribuidos.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class SistDistribuidosController {
 	@GetMapping("/hello")
 	public String saludo() {
 		return "Hola mundo";
+	}
+	
+	@GetMapping("/sumar")
+	public String sumar(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a + b);
+		return resultado;
 	}
 }
