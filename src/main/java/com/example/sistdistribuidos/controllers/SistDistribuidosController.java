25dd940a6d919d7f1ef8a8d156f7367ec9c90c11
fveloci <fveloci@hotmail.com>
2022-04-01 17:32:40 -0300

    Add online url

--- src/main/java/com/example/sistdistribuidos/controllers/SistDistribuidosController.java
+++ src/main/java/com/example/sistdistribuidos/controllers/SistDistribuidosController.java
@@ -1,11 +1,16 @@
 package com.example.sistdistribuidos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SistDistribuidosController {
+	
+	@Autowired
+	private RestTemplate clienteRest;
 	
 	@GetMapping("/hello")
 	public String saludo() {
@@ -15,12 +20,12 @@ public class SistDistribuidosController {
 	@GetMapping("/sumar")
 	public String sumar(@RequestParam float a, @RequestParam float b) {
 		String resultado = Float.toString(a + b);
-		return "El resultado es: " + resultado;
+		return clienteRest.getForObject("https://mijavaresultadosfveloci.herokuapp.com/resultado/" + resultado , String.class);
 	}
 	
 	@GetMapping("/restar")
 	public String restar(@RequestParam float a, @RequestParam float b) {
 		String resultado = Float.toString(a - b);
-		return "El resultado es: " + resultado;
+		return clienteRest.getForObject("https://mijavaresultadosfveloci.herokuapp.com/resultado/" + resultado , String.class);
 	}
 }
